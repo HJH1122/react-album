3520d3c719a04ebb9f2ef07de0e83273949836d8
hjh <ktorn21@naver.com>
2025-11-27 22:16:19 +0900

    오픈 API 호출

--- react-album/src/pages/index/index.tsx
+++ react-album/src/pages/index/index.tsx
@@ -4,8 +4,33 @@ import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar";
 import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonFooter from "@/components/common/footer/CommonFooter";
 import Card from "./components/Card";
+import axios from "axios";
+import { useEffect } from "react";
 
 function index() {
+    const getData = async () => {
+        const API_URL = "https://api.unsplash.com/search/photos";
+        const API_KEY = "EPWseG23BNY5-VOFjdlw09dAlD23w";
+
+        const PER_PAGE = 30;
+
+        const searchValue = "Korea";
+        const pageValue = 100;
+
+        try {
+            const res = await axios.get(
+                `${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`
+            );
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div className={styles.page}>
             <CommonHeader />
