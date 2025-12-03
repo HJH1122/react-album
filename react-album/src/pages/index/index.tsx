151d725209e6467c569c3e9ea7edb8ad98270c26
hjh <ktorn21@naver.com>
2025-12-03 19:57:42 +0900

    NavBar 링크이동 추가

--- react-album/src/pages/index/index.tsx
+++ react-album/src/pages/index/index.tsx
@@ -5,31 +5,57 @@ import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonFooter from "@/components/common/footer/CommonFooter";
 import Card from "./components/Card";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import type { CardDTO } from "./types/card";
 import { useRecoilValueLoadable } from "recoil";
 import { imageData } from "@/recoil/selectors/imageSelector";
 import DetailDialog from "@/components/common/dialog/DetailDialog";
 
 function index() {
-    const imgSelectorLoadable = useRecoilValueLoadable(imageData);
-    const imgSelector =
-        imgSelectorLoadable.state === "hasValue"
-            ? imgSelectorLoadable.contents
-            : null;
+    const imgSelector = useRecoilValueLoadable(imageData);
+
     const [imgData, setImgData] = useState<CardDTO>();
     const [open, setOpen] = useState<boolean>(false);
 
-    const CARD_LIST = imgSelector?.data?.results?.map((card: CardDTO) => {
-        return (
+    // const CARD_LIST = useMemo(() => {
+    //     if (imgSelector.state === "hasValue") {
+    //         const result = imgSelector.contents.map((card: CardDTO) => {
+    //             return (
+    //                 <Card
+    //                     data={card}
+    //                     key={card.id}
+    //                     handleDialog={setOpen}
+    //                     handleSetData={setImgData}
+    //                 />
+    //             );
+    //         });
+    //         return result;
+    //     } else {
+    //         return <div>loading...</div>;
+    //     }
+    // }, [imgSelector]);
+
+    const CARD_LIST = useMemo(() => {
+        if (imgSelector.state === "loading") {
+            return <div>loading...</div>;
+        }
+
+        if (imgSelector.state === "hasError") {
+            console.error(imgSelector.contents); // 에러 객체
+            return <div>데이터 로딩 실패</div>;
+        }
+
+        // 여기서는 반드시 hasValue
+        const data = imgSelector.contents ?? [];
+        return data.map((card: CardDTO) => (
             <Card
                 data={card}
                 key={card.id}
                 handleDialog={setOpen}
                 handleSetData={setImgData}
             />
-        );
-    });
+        ));
+    }, [imgSelector]);
 
     return (
         <div className={styles.page}>
