8639d57c5056c4c683072eb347b856df42dae99b
hjh <ktorn21@naver.com>
2025-12-05 20:17:52 +0900

    photo 검색 추가

--- react-album/src/components/common/searchBar/CommonSearchBar.tsx
+++ react-album/src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import styles from "./CommonSearchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/recoil/atoms/searchState";
 
 function CommonSearchBar() {
+    const [search, setSearch] = useRecoilState(searchState);
+    const [text, setText] = useState("");
+
+    const onChange = (event) => {
+        setText(event.target.value);
+    };
+
+    const onSearch = () => {
+        if (text === "") {
+            setSearch("Korea");
+        } else {
+            setSearch(text);
+        }
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === "Enter") {
+            if (text === "") {
+                setSearch("Korea");
+            } else {
+                setSearch(text);
+            }
+        }
+    };
+
     return (
         <div className={styles.searchBar}>
             <div className={styles.searchBar__search}>
@@ -8,8 +36,15 @@ function CommonSearchBar() {
                     type="text"
                     placeholder="찾으실 이미지를 검색하세요."
                     className={styles.searchBar__search__input}
+                    value={text}
+                    onChange={onChange}
+                    onKeyDown={handleKeyDown}
                 />
-                <img src="src/assets/icons/icon-search.svg" alt="" />
+                <img
+                    src="src/assets/icons/icon-search.svg"
+                    alt=""
+                    onClick={onSearch}
+                />
             </div>
         </div>
     );
